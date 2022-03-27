8f8f43489e3231c7701ef18e497874aa4569ead0
Oswaldo Peña <oswaldo@Oswaldo-Pro.local>
2022-03-26 21:30:55 -0500

    Fix error in volumen and canal limits

--- televisores/tv.py
+++ televisores/tv.py
@@ -62,17 +62,17 @@ class TV:
         return self._estado
 
     def canalUp(self):
-        if self._estado and self._canal <= 120:
+        if self._estado and self._canal < 120:
             self._canal += 1
 
     def canalDown(self):
-        if self._estado and self._canal >= 1:
+        if self._estado and self._canal > 1:
             self._canal -= 1
 
     def volumenUp(self):
-        if self._estado and self._volumen <= 7:
+        if self._estado and self._volumen < 7:
             self._volumen += 1
 
     def volumenDown(self):
-        if self._estado and self._volumen >= 1:
+        if self._estado and self._volumen > 1:
             self._volumen -= 1
